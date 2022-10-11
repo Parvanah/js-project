2e8580f445942d82960b7410343281ade4fd67ae
unknown <parvanahomarzai@gmail.com>
2022-10-11 01:12:20 -0700

    remove file

--- passing_object_function.js
+++
@@ -1,4 +0,0 @@
-function yourFunction(args){
-    let defaults = {opt1: true, opt2: 'something'};
-    let params = {...defaults, ...args}; // right-most object overwrites 
-    console.log(params.opt1);}
