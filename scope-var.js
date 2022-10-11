e1aceb331ec453b292f75fd3343ac12c467f5c8c
unknown <parvanahomarzai@gmail.com>
2022-10-11 01:13:32 -0700

    remove file

--- scope-var.js
+++
@@ -1,6 +0,0 @@
-// the scope of variables
-var myVar = "global";// Declare a global variable
-function checkscope(){
-    var myVar = "local";// Declare a local variable
-    document.write(myVar);
-}
